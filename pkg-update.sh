eee8ca76f4393f4cc6c24a490c99764d201c4035
侑 <50163161+Yuu518@users.noreply.github.com>
2024-08-28 16:43:35 +0800

    Update pkg-update.sh

--- pkg-update.sh
+++ pkg-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ddns-go
-git clone https://github.com/sirpdboy/luci-app-ddns-go luci-app-ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go ddns-go
 
 # nginx
 git clone https://github.com/sbwml/feeds_packages_net_nginx -b quic+zstd nginx
