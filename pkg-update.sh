c4cb98069b6672155fc813e8c16974024163e3a5
侑 <50163161+Yuu518@users.noreply.github.com>
2024-08-28 16:38:10 +0800

    Update pkg-update.sh

--- pkg-update.sh
+++ pkg-update.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# xray-core
-bash xray-core/update.sh
+# ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go luci-app-ddns-go
 
-# sing-box
-mkdir -p sing-box/files
-curl -Ls https://raw.githubusercontent.com/sbwml/openwrt_helloworld/v5/sing-box/files/sing-box.conf > sing-box/files/sing-box.conf
-curl -Ls https://raw.githubusercontent.com/sbwml/openwrt_helloworld/v5/sing-box/files/sing-box.init > sing-box/files/sing-box.init
-curl -Ls https://raw.githubusercontent.com/sbwml/openwrt_helloworld/v5/sing-box/Makefile > sing-box/Makefile
-
-# v2ray-data
-git clone https://github.com/pmkol/v2ray-geodata v2ray-geodata
+# nginx
+git clone https://github.com/sbwml/feeds_packages_net_nginx -b quic+zstd nginx
